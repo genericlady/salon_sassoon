e18e3fb0812eaa901511edc29790298bc22bb70e
Lady Codington <myemailforcoding@gmail.com>
2016-04-01 22:54:18 -0400

    CRUD interface done for stylists not tested

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ActiveRecord::Base
   belongs_to :user
   belongs_to :stylist
+
+  def self.valid_params?(params)
+    !params[:start].empty? && !params[:finish] && !params[:date]
+  end
 end
--- app/models/stylist.rb
+++ app/models/stylist.rb
@@ -1,4 +1,8 @@
 class Stylist < ActiveRecord::Base
   has_many :appointments
   has_many :users, through: :appointments
+
+  def self.valid_params?(params)
+    !params[:name].empty?
+  end
 end
